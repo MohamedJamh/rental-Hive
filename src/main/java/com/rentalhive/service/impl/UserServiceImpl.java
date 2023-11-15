81a66d4834cef242de227aacc2773d8e8b07bc77
MohamedJamh <jam7.mohamed@gmail.com>
2023-11-15 17:08:30 +0100

    main: add/delete - delete security dependency to bypass default login layer, adding simple example for the rest controllers

--- src/main/java/com/rentalhive/service/impl/UserServiceImpl.java
+++ src/main/java/com/rentalhive/service/impl/UserServiceImpl.java
@@ -2,9 +2,10 @@ package com.rentalhive.service.impl;
 
 import com.rentalhive.domain.User;
 import com.rentalhive.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class UserServiceImpl implements UserService {
     @Override
     public User save(User user) {
--- src/main/java/com/rentalhive/web/rest/UserRest.java
+++ src/main/java/com/rentalhive/web/rest/UserRest.java
@@ -1,9 +1,34 @@
 package com.rentalhive.web.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rentalhive.domain.User;
+import com.rentalhive.service.impl.UserServiceImpl;
+import com.rentalhive.utils.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/user")
 public class UserRest {
+
+    private final UserServiceImpl userService;
+    @Autowired
+    public UserRest(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Response<User>> save(@RequestParam String firstName){
+        Response<User> userResponse = new Response<>();
+        userResponse.setMessage("yest");
+        userResponse.setResult(
+                User.builder()
+                        .firstName(firstName)
+                        .build()
+        );
+        return new ResponseEntity<>(
+                userResponse,HttpStatus.OK
+        );
+    }
 }
