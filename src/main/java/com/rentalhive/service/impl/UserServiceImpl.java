626d4338bb76dc91d8f054e84d5f710c147c7929
MohamedJamh <jam7.mohamed@gmail.com>
2023-11-16 15:25:52 +0100

    userFeature: add - working on user feature

--- src/main/java/com/rentalhive/service/impl/UserServiceImpl.java
+++ src/main/java/com/rentalhive/service/impl/UserServiceImpl.java
@@ -1,15 +1,26 @@
 package com.rentalhive.service.impl;
 
 import com.rentalhive.domain.User;
+import com.rentalhive.repository.UserRepository;
 import com.rentalhive.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     @Override
     public User save(User user) {
-        return null;
+        String organizationName = user.getOrganization().getName();
+
+        return userRepository.save(user);
     }
     @Override
     public User update(User user) {
@@ -23,6 +34,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> findAll() {
-        return null;
+        return new ArrayList<>();
     }
 }
--- src/main/java/com/rentalhive/web/rest/UserRest.java
+++ src/main/java/com/rentalhive/web/rest/UserRest.java
@@ -1,12 +1,16 @@
 package com.rentalhive.web.rest;
 
 import com.rentalhive.domain.User;
+import com.rentalhive.dto.UserDto;
+import com.rentalhive.mapper.UserDtoMapper;
 import com.rentalhive.service.impl.UserServiceImpl;
 import com.rentalhive.utils.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -18,17 +22,13 @@ public class UserRest {
         this.userService = userService;
     }
 
-    @GetMapping
-    public ResponseEntity<Response<User>> save(@RequestParam String firstName){
+
+    @PostMapping
+    public ResponseEntity<String> save(@RequestBody @Valid UserDto userDto){
         Response<User> userResponse = new Response<>();
-        userResponse.setMessage("yest");
-        userResponse.setResult(
-                User.builder()
-                        .firstName(firstName)
-                        .build()
-        );
-        return new ResponseEntity<>(
-                userResponse,HttpStatus.OK
-        );
+        User user = UserDtoMapper.toEntity(userDto);
+        userResponse.setMessage("User saved successfully");
+        userResponse.setResult(userService.save(UserDtoMapper.toEntity(userDto)));
+        return new ResponseEntity<>(userResponse.toString(), HttpStatus.OK);
     }
 }
