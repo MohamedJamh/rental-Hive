d736ecfe7c6e319c78c2f37e73eb26e9e15f6b03
MohamedJamh <jam7.mohamed@gmail.com>
2023-11-26 20:43:43 +0100

    offerFeature: add - nested OrderEquipment inside order wich is inside offer dto to submit rent price for each equipment base on user order , as well as mapper

--- src/main/java/com/rentalhive/dto/OfferDto.java
+++ src/main/java/com/rentalhive/dto/OfferDto.java
@@ -1,12 +1,15 @@
 package com.rentalhive.dto;
 
+import com.rentalhive.dto.request.OrderEquipmentRequestDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -26,4 +29,8 @@ public class OfferDto {
 
     @NotNull(message = "order is required")
     private Long orderId;
+
+    @NotNull(message = "order equipments are required")
+    @Valid
+    private List<OrderEquipmentRequestDto> orderEquipments;
 }
